0213e7da4880772049363510843c149e8412681c
Your Name <email@example.com>
2020-03-28 16:10:14 +0800

    更改日志名称注释

--- run_suite.py
+++ run_suite.py
@@ -3,12 +3,10 @@ from scripts.test_ihrm_login import TestIHRMLogin
 from HTMLTestRunner_PY3 import HTMLTestRunner
 import time
 from app import BASE_DIR
-# 创建测试套件
+
 suite = unittest.TestSuite()
-# 添加测试用例到测试套件
+
 suite.addTest(unittest.makeSuite(TestIHRMLogin))
-# 使用runner来运行测试套件
-# 定义测试报告的文件名称
 
 reportname = BASE_DIR + "/report/report.html"
 with open(reportname,'wb') as f:
