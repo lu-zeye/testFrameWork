6436f097515f0199ca0a336830392bcfe204c554
Your Name <email@example.com>
2020-03-28 15:45:41 +0800

    更改日志名称

--- run_suite.py
+++ run_suite.py
@@ -9,7 +9,8 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestIHRMLogin))
 # 使用runner来运行测试套件
 # 定义测试报告的文件名称
-reportname = BASE_DIR + "/report/report {}.html".format(time.strftime("%Y%m%d %H%M%S"))
+# reportname = BASE_DIR + "/report/report {}.html".format(time.strftime("%Y%m%d %H%M%S"))
+reportname = BASE_DIR + "/report/report.html"
 with open(reportname,'wb') as f:
     runner = HTMLTestRunner(f,verbosity=2,description="这是测试报告",title="IHRM报告")
     runner.run(suite)
